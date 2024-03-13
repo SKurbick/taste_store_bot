d444efe2569a0e69b79b90d0e4b5663e8ae48da2
SKurbick <malkolm.63.zed@mail.ru>
2024-03-13 22:17:57 +0300

    [*]timezone and time

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ async def main() -> None:
     logger.info("STARTING BOT")
 
     scheduler = AsyncIOScheduler()
-    scheduler.add_job(for_admin_message, 'cron', hour=16, minute=16, args=(bot,))
+    scheduler.add_job(for_admin_message, 'cron', hour=23, minute=00, args=(bot,), timezone='Europe/Moscow')
 
     dp.include_router(registration_handlers.router)
 
